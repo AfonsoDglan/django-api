a2c58e47d2bf2e9b1ae40000f8289ce8b0d05d62
afonso dglan <dglan2003@gmail.com>
2025-10-05 19:43:29 -0400

    remove function based view this project

--- api/views.py
+++ api/views.py
@@ -13,34 +13,16 @@ class ProductListAPIView(generics.ListAPIView):
     queryset = Product.objects.exclude(stock__gt=0)
     serializer_class = ProductSerializer
 
-# @api_view(['GET'])
-# def product_list(request):
-#     products = Product.objects.all()
-#     serializer = ProductSerializer(products, many=True)
-#     return Response(serializer.data)
-
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_url_kwarg = 'product_id'
 
-# @api_view(['GET'])
-# def product_detail(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     serializer = ProductSerializer(product)
-#     return Response(serializer.data)
-
 
 class OrderListAPIView(generics.ListAPIView):
     queryset = Order.objects.prefetch_related('items__product')
     serializer_class = OrderSerializer
-
-# @api_view(['GET'])
-# def order_list(request):
-#     orders = Order.objects.prefetch_related('items__product')
-#     serializer = OrderSerializer(orders, many=True)
-#     return Response(serializer.data)
 
 
 @api_view(['GET'])
