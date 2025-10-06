91067489696f24f6ac9c071adeb60e36e9ce84c8
afonso dglan <dglan2003@gmail.com>
2025-10-05 22:13:51 -0400

    replace decorator api_view in view procts_info with apiview class

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('products/', views.ProductListAPIView.as_view()),
-    path('products/info/', views.product_info),
-    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view()),
-    path('orders/', views.OrderListAPIView.as_view()),
+    path('products/', views.ProductListAPIView.as_view(), name='products'),
+    path('products/info/', views.ProductInfoAPIView.as_view(), name='products-info'),
+    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
+    path('orders/', views.OrderListAPIView.as_view(), name='orders'),
     path('user-orders/', views.UserOrderListAPIView.as_view(), name='user-orders'),
 ]
